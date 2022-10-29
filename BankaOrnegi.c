80375eca448cfb7e939629c8d4f1f8b239b7ac31
burakqull <83167665+burakqull@users.noreply.github.com>
2022-10-29 22:35:58 +0300

    Update BankaOrnegi.c

--- BankaOrnegi.c
+++ BankaOrnegi.c
@@ -2,35 +2,20 @@
 #include <stdlib.h>
 #include "BankaOrnegi.h"
 
-
 int main(){
 
 
     insan* dizi=NULL;
-    
+
     dizi=kisiEkle("Betül","Köse",1,dizi);
     dizi=kisiEkle("Burak","Gül",1,dizi);
-    dizi=kisiEkle("Batuhan","Gül",1,dizi);
-    dizi=kisiEkle("Batuhan","Köse",0,dizi);
+    listeyiYazdir(dizi);
 
+    printf("\n\n\n");
+    dizi=kisiEkle("Batuhan","Gül",0,dizi);
     listeyiYazdir(dizi);
     dizi=kisiCagir(dizi);
-    dizi=kisiCagir(dizi);
-    dizi=kisiCagir(dizi);
-    dizi=kisiCagir(dizi);
-
-
-    
-
-
-
-    
-    
-
-
-
-
-
-
+    printf("\n liste \n");
+    listeyiYazdir(dizi);
     return 0 ;
 }
